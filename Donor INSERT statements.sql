0908e605da7569915162f20fcd776303d21b8ee0
ayokunlejames <129544207+ayokunlejames@users.noreply.github.com>
2023-06-30 18:07:09 +0100

    Add files via upload

---
+++ Donor INSERT statements.sql
@@ -0,0 +1,45 @@
+INSERT INTO "humanitarianOrg".donor(
+	donorid, donorname, donorgender, donorlocation, donorjobfield)
+	VALUES ('A', 'Ingo Fernandez', 'Male', 'United Kingdom', 'Education');
+		
+INSERT INTO "humanitarianOrg".donor(
+	donorid, donorname, donorgender, donorlocation, donorjobfield)
+	VALUES ('B','Vinita Bach','Female','India','Engineering');
+	
+INSERT INTO "humanitarianOrg".donor(
+	donorid, donorname, donorgender, donorlocation, donorjobfield)
+	VALUES ('C','Ayo James','Female','Nigeria','IT');
+	
+INSERT INTO "humanitarianOrg".donor(
+	donorid, donorname, donorgender, donorlocation, donorjobfield)
+	VALUES ('D','Kunle John','Female','Nigeria','Healthcare');
+			
+INSERT INTO "humanitarianOrg".donor(
+	donorid, donorname, donorgender, donorlocation, donorjobfield)
+	VALUES ('E','titi grace','Male','Zimbabwe','IT');
+	
+INSERT INTO "humanitarianOrg".donor(
+	donorid, donorname, donorgender, donorlocation, donorjobfield)
+	VALUES ('H','michael jackson','Male','Poland','Sales');
+	
+INSERT INTO "humanitarianOrg".donor(
+	donorid, donorname, donorgender, donorlocation, donorjobfield)
+	VALUES ('I','Fela Kuti','Male','Australia','Human Resources');
+	
+INSERT INTO "humanitarianOrg".donor(
+	donorid, donorname, donorgender, donorlocation, donorjobfield)
+	VALUES ('J','Bob Marley','Female','New Zealand','Business');
+	
+INSERT INTO "humanitarianOrg".donor(
+	donorid, donorname, donorgender, donorlocation, donorjobfield)
+	VALUES ('K','josiah Ayo','Female','Canada','Healthcare');
+	
+
+INSERT INTO "humanitarianOrg".donor(
+	donorid, donorname, donorgender, donorlocation, donorjobfield)
+	VALUES ('M','banji samuel','Male','USA','Business');
+			
+INSERT INTO "humanitarianOrg".donor(
+	donorid, donorname, donorgender, donorlocation, donorjobfield)
+	VALUES ('N','lemy emmanuel','Female','USA','IT');
+	
